294de9ff38fdd3370ca46e88f9f3b28ebbceb8a8
reda <redabouzidi62@yahoo.fr>
2023-05-01 01:42:50 +0200

    modifications sur les amis ( niveau bdd ) (fait avec cenk et sidy)

--- Serveur/Serveur_Bdd/Amis.cs
+++ Serveur/Serveur_Bdd/Amis.cs
@@ -38,15 +38,12 @@ class Amis
 
 
 
-    public void send_friend_request(MySqlConnection conn,int sender,int receiver){//envoyer une demande
+    public static int send_friend_request(MySqlConnection conn,int sender,int receiver){//envoyer une demande
         try{
             using(MySqlCommand command=new MySqlCommand()){
-                string query="SELECT count(*) FROM Amis WHERE idUsers1=@ids AND idUsers2=@idu";
+                string query="SELECT count(*) FROM Amis WHERE (idUsers1=@ids AND idUsers2=@idu) OR (idUsers2=@ids AND idUsers1=@idu)";
                 int rowcount=conn.QueryFirstOrDefault<int>(query,new{ids=sender,idu=receiver});
-                if(rowcount>0)return;
-                query="SELECT count(*) FROM Amis WHERE idUsers2=@ids AND idUsers1=@idu";
-                rowcount=conn.QueryFirstOrDefault<int>(query,new{ids=sender,idu=receiver});
-                if(rowcount>0)return;
+                if(rowcount>0)return 0;
                 //Attribuer la connection à la commande
                 command.Connection=conn;
                 //Inserer dans la BDD, Creation d'un lien amitié
@@ -59,12 +56,19 @@ class Amis
                 //Execution
                 rowcount=command.ExecuteNonQuery();
                 //si rowcount (ligne inseré) = 0
-                if(rowcount==0)Console.WriteLine("Failed while inserting");
-                else Console.WriteLine("Success");
+                if(rowcount==0){
+                    Console.WriteLine("Failed while inserting");
+                    return 2;
+                }
+                else {
+                    return 1;
+                    Console.WriteLine("Success");
+                }
             }
         }
         catch(Exception e){
             Console.WriteLine(e.Message);
+            return 3;
         }
     }
 
@@ -167,35 +171,13 @@ class Amis
         //retour
         return (identifiants,pseudos,dates);
     }
-    public static (int[],string[]) search_for_player(MySqlConnection conn,string name_player){
-        string query="SELECT idUsers,pseudo FROM Utilisateurs WHERE pseudo LIKE @NP";
-        List<(int id, string username)> data = conn.Query<(int, string)>(query, new { NP = "%" + name_player + "%" }).ToList();
+    public static (int[],string[]) search_for_player(MySqlConnection conn,int idUsers,string name_player){
+        string query="SELECT U.idUsers,U.pseudo FROM Utilisateurs U WHERE U.pseudo LIKE @NP AND U.idUsers NOT IN (SELECT A.idUsers2 FROM Amis A WHERE A.idUsers1 = @id) AND U.idUsers NOT IN (SELECT A.idUsers1 FROM Amis A WHERE A.idUsers2 = @id) AND U.idUsers != @id";
+        List<(int id, string username)> data = conn.Query<(int, string)>(query, new { NP = "%" + name_player + "%" ,id=idUsers}).ToList();
         string[] username = data.Select(r=> r.username).ToArray();
         int[] id = data.Select(r => r.id).ToArray();
         return (id,username);
     }
-	public List<string> searchNonAmis(MySqlConnection conn, int idUsers)
-    {
-        List<string> result = new List<string>();
-
-        using (MySqlCommand command = new MySqlCommand())
-        {
-            command.Connection = conn;
-            command.CommandText = "SELECT U.pseudo FROM Utilisateurs U " +
-                            "WHERE U.idUsers NOT IN " +
-                            "(SELECT A.idUsers2 FROM Amis A WHERE A.idUsers1 = @id) " +
-                            "AND U.idUsers != @id;";
-            command.Parameters.AddWithValue("@id", idUsers);
-
-            using (MySqlDataReader reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    result.Add(reader["pseudo"].ToString());
-                }
-            }
-        }
-	}
     public static int get_friend_count(MySqlConnection conn,int id_player){
         int friend_count=0;
         //Recuperer le nombre d'amis d'un joueur
