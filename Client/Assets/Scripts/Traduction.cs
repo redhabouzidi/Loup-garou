e9a1731b9180cb5130a8f957eeffe970fc89b045
jennifer klotz <jennifer.klotz@etu.unistra.fr>
2023-05-05 10:50:58 +0200

    intégration des valeurs des sliders, modification des boutons langues, ajout info bulle pour le pseudo + modifications mineures

--- Client/Assets/Scripts/Traduction.cs
+++ Client/Assets/Scripts/Traduction.cs
@@ -9,11 +9,15 @@ using Assets.SimpleLocalization;
 	public class Traduction : MonoBehaviour
 	{
         public Button FrenchB,EnglishB;
+		private Image FrenchI, EnglishI;
 		/// <summary>
 		/// Called on app start.
 		/// </summary>
 		void Start()
 		{
+			FrenchI = FrenchB.gameObject.GetComponent<Image>();
+			EnglishI = EnglishB.gameObject.GetComponent<Image>();
+
 			LocalizationManager.Read();
             FrenchB.onClick.AddListener(()=>SetLocalizationFr());
             EnglishB.onClick.AddListener(()=>SetLocalizationEn());
@@ -21,8 +25,12 @@ using Assets.SimpleLocalization;
         void SetLocalizationFr()
 		{
 			LocalizationManager.Language = "French";
+			FrenchI.fillCenter = true;
+			EnglishI.fillCenter = false;
 		}
         void SetLocalizationEn(){
             LocalizationManager.Language = "English";
+			FrenchI.fillCenter = false;
+			EnglishI.fillCenter = true;
         }
 	}
