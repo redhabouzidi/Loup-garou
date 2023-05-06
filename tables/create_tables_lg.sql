8d04cbb2950c80a00467c3d7dc97952d88ad889a
tombala78 <cenk.colakoglu250@gmail.com>
2023-05-07 00:28:09 +0200

    Modification finale des classes Amis,Statistiques et la creation des tables

--- tables/create_tables_lg.sql
+++ tables/create_tables_lg.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS Utilisateurs (
   pseudo VARCHAR(50) NOT NULL UNIQUE,
   motdepasse VARCHAR(128) NOT NULL,
   CONSTRAINT CK_UTILISATEURS_EMAIL CHECK (email REGEXP ("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")),
-  CONSTRAINT CK_PSEUDO CHECK (pseudo REGEXP "^[a-zA-Z][a-zA-Z0-9_]{5,50}$")
+  CONSTRAINT CK_PSEUDO CHECK (pseudo REGEXP "^[a-zA-Z][a-zA-Z0-9_]{4,50}$")
 );
 
 CREATE TABLE IF NOT EXISTS Amis(
@@ -20,30 +20,29 @@ CREATE TABLE IF NOT EXISTS Amis(
 
 CREATE TABLE IF NOT EXISTS Partie(
 	idPartie INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    Datesauvegarde DATETIME,
 	nomPartie VARCHAR(255) NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS SauvegardePartie(
 	idPartie INT,
 	idUsers INT,
-	Datesauvegarde DATE,
+    scoregained INT DEFAULT 0,
 	PRIMARY KEY(idUsers, idPartie)
 );
 
 CREATE TABLE IF NOT EXISTS Actions (
-	idAction INT AUTO_INCREMENT PRIMARY KEY,
 	idPartie INT,
-	act_temps TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	actions VARCHAR(1024)
+	actions TEXT,
+    actionsEn TEXT
 );
 CREATE TABLE IF NOT EXISTS Statistiques(
-   idStatistiques INT PRIMARY KEY, 
+   idStatistiques INT AUTO_INCREMENT PRIMARY KEY, 
    idUsers INT,
    score INT NOT NULL DEFAULT 0,
    nb_partiejoue INT NOT NULL DEFAULT 0,
    nb_victoire INT NOT NULL DEFAULT 0
 );
-
 
 ALTER TABLE Amis ADD CONSTRAINT FK_Amis_idUsers1 FOREIGN KEY (idUsers1) REFERENCES Utilisateurs (idUsers) ON DELETE CASCADE;
 ALTER TABLE Amis ADD CONSTRAINT FK_Amis_idUsers2 FOREIGN KEY (idUsers2) REFERENCES Utilisateurs (idUsers) ON DELETE CASCADE;
@@ -51,5 +50,6 @@ ALTER TABLE Amis ADD CONSTRAINT FK_Amis_idUsers2 FOREIGN KEY (idUsers2) REFERENC
 ALTER TABLE SauvegardePartie ADD CONSTRAINT FK_Sauvegarde_IdUsers FOREIGN KEY (IdUsers) REFERENCES Utilisateurs (IdUsers) ON DELETE CASCADE;
 ALTER TABLE SauvegardePartie ADD CONSTRAINT FK_Sauvegarde_IdPartie FOREIGN KEY (IdPartie) REFERENCES Partie (IdPartie) ON DELETE CASCADE;
 
+ALTER TABLE Actions ADD CONSTRAINT FK_Actions_IdPartie FOREIGN KEY (IdPartie) REFERENCES Partie (IdPartie) ON DELETE CASCADE;
 
-ALTER TABLE Statistiques ADD CONSTRAINT FK_Statistiques_idPartie FOREIGN KEY (idUsers) REFERENCES Utilisateurs (IdUsers) ON DELETE CASCADE;
+ALTER TABLE Statistiques ADD CONSTRAINT FK_Statistiques_idUser FOREIGN KEY (idUsers) REFERENCES Utilisateurs (IdUsers) ON DELETE CASCADE;
--- versionBeta/Amis.cs
+++ versionBeta/Amis.cs
@@ -5,10 +5,10 @@ using Dapper;
 using System.Linq;
 using System.Collections.Generic;
 
+class Amis
+{
 
-class Amis{
-    //Fonction permettant de recuperer l'identifiant d'un joueur a partir de son pseudo
-    public int get_id(MySqlConnection conn,string pseudo){
+    public static int get_id(MySqlConnection conn,string pseudo){
         try{
             //Requete pour recuperer identifiant du joueur
             string query="SELECT idUsers FROM Utilisateurs WHERE pseudo=@Pseudo";
@@ -21,8 +21,8 @@ class Amis{
             return -1;
         }
     }
-    //Fonction permettant de recuperer le pseudo d'un joueur a partir de son identifiant
-    public string get_pseudo(MySqlConnection conn,int idplayer){
+
+    public static string get_pseudo(MySqlConnection conn,int idplayer){
         try{
             //requete pour recuperer le pseudo d'un joueur
             string query="SELECT pseudo FROM Utilisateurs WHERE idUsers=@IDJ";
@@ -35,12 +35,15 @@ class Amis{
             return "";
         }
     }
-    //Fonction permettant d'envoyer une demande amis à une personne
-    //sender -> qui envoie la demande
-    //receiver -> qui reçoit la demande
-    public void send_friend_request(MySqlConnection conn,int sender,int receiver){//envoyer une demande
+
+
+
+    public static int send_friend_request(MySqlConnection conn,int sender,int receiver){//envoyer une demande
         try{
             using(MySqlCommand command=new MySqlCommand()){
+                string query="SELECT count(*) FROM Amis WHERE (idUsers1=@ids AND idUsers2=@idu) OR (idUsers2=@ids AND idUsers1=@idu)";
+                int rowcount=conn.QueryFirstOrDefault<int>(query,new{ids=sender,idu=receiver});
+                if(rowcount>0)return 0;
                 //Attribuer la connection à la commande
                 command.Connection=conn;
                 //Inserer dans la BDD, Creation d'un lien amitié
@@ -51,20 +54,27 @@ class Amis{
                 command.Parameters.AddWithValue("@STA",false);
                 command.Parameters.AddWithValue("@DA",DateTime.Now);
                 //Execution
-                int rowcount=command.ExecuteNonQuery();
+                rowcount=command.ExecuteNonQuery();
                 //si rowcount (ligne inseré) = 0
-                if(rowcount==0)Console.WriteLine("Failed while inserting");
-                else Console.WriteLine("Success");
+                if(rowcount==0){
+                    Console.WriteLine("Failed while inserting");
+                    return 2;
+                }
+                else {
+                    return 1;
+                    Console.WriteLine("Success");
+                }
             }
         }
         catch(Exception e){
             Console.WriteLine(e.Message);
+            return 3;
         }
     }
-    //Fonction permettant d'accepter la demande ami
-    //playeraccepte -> qui accepte la demande
-    //playersend -> qui a envoye la demande
-    public void accept_friend_request(MySqlConnection conn,int playeraccepte,int playersend){//accepter la demande
+
+
+
+    public static void accept_friend_request(MySqlConnection conn,int playeraccepte,int playersend){//accepter la demande
         try{
             using(MySqlCommand command=new MySqlCommand()){
                 //Attribuer la connection à la commande
@@ -87,10 +97,10 @@ class Amis{
             Console.WriteLine(e.Message);
             }
         }
-    //Fonction permettant de refuser une demande ami ou supprimer un ami
-    //playerrefuse -> qui refuse la demande/supprime un ami
-    //playersender -> qui a envoye la demande
-    public void refuse_friend_request(MySqlConnection conn,int playerrefuse,int playersender){//refuser la demande ou supprimer
+
+
+
+    public static void refuse_friend_request(MySqlConnection conn,int playerrefuse,int playersender){//refuser la demande ou supprimer
         try{
             using(MySqlCommand command=new MySqlCommand()){
                 //Attribuer la connection à la commande
@@ -111,48 +121,64 @@ class Amis{
             Console.WriteLine(e.Message);
         }
     }
-//Fonction permettant de recuperer tous les  amis d'une personne
-public (int[],string[],DateTime[]) get_liste_amis(MySqlConnection conn,int idPlayer){
-        //Recuperer les amis qui a reçu une demande de joueur idPlayer
-        string query="SELECT idUsers2,date_amis FROM Amis WHERE idUsers1=@IDF AND status_ami=@SA;";
-        List<(int id,DateTime date)> data_f=conn.Query<(int,DateTime)>(query,new{IDF=idPlayer,SA=true}).ToList();
-        //Recuperer les amis qui a envoyé une demande au joueur idPlayer
-        query="SELECT idUsers1,date_amis FROM Amis WHERE idUsers2=@IDF AND status_ami=@SA;";
-        List<(int id,DateTime date)> data_s=conn.Query<(int,DateTime)>(query,new{IDF=idPlayer,SA=true}).ToList();
-        //concatenation des listes reçu
-        int[] identifiants = data_f.Select(r => r.id).Concat(data_s.Select(r=>r.id)).ToArray();
-        DateTime[] dates = data_f.Select(r => r.date).Concat(data_s.Select(r=>r.date)).ToArray();
-        //Recuperer les pseudos a partir du liste des identifiants
-        query = "SELECT pseudo FROM Utilisateurs WHERE idUsers IN @IDS ORDER BY FIELD(idUsers, " + string.Join(",", identifiants) + ")";
-        string[]pseudos=conn.Query<string>(query,new{IDS=identifiants}).ToArray();
-        //retour
-        return (identifiants,pseudos,dates);
-    }   
 
-//Fonction permettant de recuperer tous les demande amis en attente d'une personne 
-public (int[],string[],DateTime[]) get_liste_amis_enattente(MySqlConnection conn,int idPlayer){
+
+
+    public static (int[], string[], DateTime[]) get_liste_amis(MySqlConnection conn, int idPlayer)
+    {
+        try
+        {
+            //Recuperer les amis qui a reçu une demande de joueur idPlayer
+            string query = "SELECT idUsers2,date_amis FROM Amis WHERE idUsers1=@IDF AND status_ami=@SA;";
+            List<(int id, DateTime date)> data_f = conn.Query<(int, DateTime)>(query, new { IDF = idPlayer, SA = true }).ToList();
+            //Recuperer les amis qui a envoyé une demande au joueur idPlayer
+            query = "SELECT idUsers1,date_amis FROM Amis WHERE idUsers2=@IDF AND status_ami=@SA;";
+            List<(int id, DateTime date)> data_s = conn.Query<(int, DateTime)>(query, new { IDF = idPlayer, SA = true }).ToList();
+            //concatenation des listes reçu
+            int[] identifiants = data_f.Select(r => r.id).Concat(data_s.Select(r => r.id)).ToArray();
+            DateTime[] dates = data_f.Select(r => r.date).Concat(data_s.Select(r => r.date)).ToArray();
+            //Recuperer les pseudos a partir du liste des identifiants
+            query = "SELECT pseudo FROM Utilisateurs WHERE idUsers IN @IDS ORDER BY FIELD(idUsers, " + string.Join(",", identifiants) + ")";
+            string[] pseudos = conn.Query<string>(query, new { IDS = identifiants }).ToArray();
+            //retour
+            return (identifiants, pseudos, dates);
+        }
+        catch (Exception e)
+        {
+            return (new int[0], new string[0], new DateTime[0]);
+        }
+    }
+
+    public static (int[],string[],DateTime[]) get_liste_amis_enattente(MySqlConnection conn,int idPlayer){
         //Recuperer les amis qui a reçu une demande de joueur idPlayer
         string query="SELECT idUsers2,date_amis FROM Amis WHERE idUsers1=@IDF AND status_ami=@SA";
         List<(int id,DateTime date)> data_f=conn.Query<(int,DateTime)>(query,new{IDF=idPlayer,SA=false}).ToList();
+        data_f.Add((-1,new DateTime()));
         //Recuperer les amis qui a envoyé une demande au joueur idPlayer
-        query="SELECT idUsers2,date_amis FROM Amis WHERE idUsers2=@IDF AND status_ami=@SA";
+        query="SELECT idUsers1,date_amis FROM Amis WHERE idUsers2=@IDF AND status_ami=@SA";
         List<(int id,DateTime date)> data_s=conn.Query<(int,DateTime)>(query,new{IDF=idPlayer,SA=false}).ToList();
         //concatenation des listes reçu
         int[] identifiants = data_f.Select(r => r.id).Concat(data_s.Select(r=>r.id)).ToArray();
         DateTime[] dates = data_f.Select(r => r.date).Concat(data_s.Select(r=>r.date)).ToArray();
         //Recuperer les pseudos a partir du liste des identifiants
         query = "SELECT pseudo FROM Utilisateurs WHERE idUsers IN @IDS ORDER BY FIELD(idUsers, " + string.Join(",", identifiants) + ")";
-        string[]pseudos=conn.Query<string>(query,new{IDS=identifiants}).ToArray();
+        string[]temp=conn.Query<string>(query,new{IDS=identifiants}).ToArray();
+        Console.WriteLine(string.Join(",", identifiants));
+        string[]pseudos = new string[temp.Length + 1];
+        Array.Copy(temp, 0, pseudos, 0, data_f.Count-1);
+        pseudos[data_f.Count-1] = "";
+        Array.Copy(temp, data_f.Count-1, pseudos, data_f.Count, data_s.Count);
         //retour
         return (identifiants,pseudos,dates);
     }
-    //Fonction pour rechercher des personnes ayant un nom similaire ou exacte au nom passé en parametre
-    public string[] search_for_player(MySqlConnection conn,string name_player){
-        string query="SELECT pseudo FROM Utilisateurs WHERE pseudo LIKE @NP";
-        string[] data=conn.Query<string>(query,new{NP = "%" + name_player + "%"}).ToArray();
-        return data;
+    public static (int[],string[]) search_for_player(MySqlConnection conn,int idUsers,string name_player){
+        string query="SELECT U.idUsers,U.pseudo FROM Utilisateurs U WHERE U.pseudo LIKE @NP AND U.idUsers NOT IN (SELECT A.idUsers2 FROM Amis A WHERE A.idUsers1 = @id) AND U.idUsers NOT IN (SELECT A.idUsers1 FROM Amis A WHERE A.idUsers2 = @id) AND U.idUsers != @id";
+        List<(int id, string username)> data = conn.Query<(int, string)>(query, new { NP = "%" + name_player + "%" ,id=idUsers}).ToList();
+        string[] username = data.Select(r=> r.username).ToArray();
+        int[] id = data.Select(r => r.id).ToArray();
+        return (id,username);
     }
-    public int get_friend_count(MySqlConnection conn,int id_player){
+    public static int get_friend_count(MySqlConnection conn,int id_player){
         int friend_count=0;
         //Recuperer le nombre d'amis d'un joueur
         //soit ses amis qui ont recu la demande de ce joueur
@@ -161,34 +187,4 @@ public (int[],string[],DateTime[]) get_liste_amis_enattente(MySqlConnection conn
         friend_count=conn.QueryFirstOrDefault<int>(query,new{IDS=id_player,IDF=id_player,SA=true});
         return friend_count;
     }
-
-
-    //fonction qui permet de faire la recherche des personnes qui ne sont pas amis avec l'utilisateurs!!!
-    
-    public List<string> searchNonAmis(MySqlConnection conn, int idUsers)
-    {
-        List<string> result = new List<string>();
-
-        using (MySqlCommand command = new MySqlCommand())
-        {
-            command.Connection = conn;
-            command.CommandText = "SELECT U.pseudo FROM Utilisateurs U " +
-                            "WHERE U.idUsers NOT IN " +
-                            "(SELECT A.idUsers2 FROM Amis A WHERE A.idUsers1 = @id) " +
-                            "AND U.idUsers != @id;";
-            command.Parameters.AddWithValue("@id", idUsers);
-
-            using (MySqlDataReader reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    result.Add(reader["pseudo"].ToString());
-                }
-            }
-        }
-
-        return result;
-}
-
-
 }
