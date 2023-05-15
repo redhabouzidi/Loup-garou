e132fd79d2cf2d0adb8b475b75e20576fa810a1b
tombala78 <cenk.colakoglu250@gmail.com>
2023-05-15 12:44:20 +0200

    traduction waitingscreen,&gamescene

--- Client/Assets/GameManager.cs
+++ Client/Assets/GameManager.cs
@@ -1,4 +1,4 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -73,6 +73,9 @@ public class GameManager : MonoBehaviour
 
     public AudioSource soundManager_day,soundManager_night;
 
+    private bool isfr;
+    public Button to_french,to_english;
+    public GameObject winscreencontainer;
     // Start is called before the first frame update
     void Start()
     {
@@ -95,6 +98,9 @@ public class GameManager : MonoBehaviour
         buttonLeaveGame.onClick.AddListener(OnButtonClickLeaveGame);
         buttonPlayAgain.onClick.AddListener(OnButtonClickPlayAgain);
         sePresenter.onClick.AddListener(OnButtonClickSePresenter);
+        to_english.onClick.AddListener(translateanglais);
+        to_french.onClick.AddListener(translatefrench);
+        
         soundNight=true;
         play_sound_night();
         NetworkManager.gm = GameObject.Find("GameManager").GetComponent<GameManager>();
@@ -278,6 +284,79 @@ public class GameManager : MonoBehaviour
 
     }
 
+    public void translatefrench(){
+        isfr=true;
+        switch(isVillageWin) {
+            case 2 : // Loup-garou
+                groupWin.text = "Les loups-garous ont gagné";
+                groupWin.color = colorRed;
+                break;
+            
+            case 1 : // villageois
+                groupWin.text = "Les villageois ont gagné";
+                groupWin.color = colorWhite;
+                break;
+            
+            case 4 : // draw
+                groupWin.text = "Match nul";
+                groupWin.color = colorWhite;
+                break;
+
+            case 3 : // amoureux
+                groupWin.text = "Les amoureux ont gagné";
+                groupWin.color = colorPink;
+                break;
+        }
+        TextMeshProUGUI[] textFields = winscreencontainer.GetComponentsInChildren<TextMeshProUGUI>(true);
+        foreach(TextMeshProUGUI textf in textFields){
+            Debug.Log("sdkjaskdjasdkasjdksa"+textf.text);
+            textf.text=textf.text.Replace("Werewolf","Loup-garou");
+            textf.text=textf.text.Replace("Seer","Voyante");
+            textf.text=textf.text.Replace("Dictator","Dictateur");
+            textf.text=textf.text.Replace("Cupid","Cupidon");
+            textf.text=textf.text.Replace("Villager","Villageois");
+            textf.text=textf.text.Replace("Witch","Sorciere");
+            textf.text=textf.text.Replace("Hunter","Chasseur");
+            textf.text=textf.text.Replace("Guard","Garde");
+        }
+    }
+    public void translateanglais(){
+        isfr=false;
+        switch(isVillageWin) {
+            case 2 : // Loup-garou
+                groupWin.text = "Werewolves won";
+                groupWin.color = colorRed;
+                break;
+            
+            case 1 : // villageois
+                groupWin.text = "Villagers won";
+                groupWin.color = colorWhite;
+                break;
+            
+            case 4 : // draw
+                groupWin.text = "Draw";
+                groupWin.color = colorWhite;
+                break;
+
+            case 3 : // amoureux
+                groupWin.text = "Lovers won";
+                groupWin.color = colorPink;
+                break;
+        }
+        TextMeshProUGUI[] textFields = winscreencontainer.GetComponentsInChildren<TextMeshProUGUI>(true);
+        foreach(TextMeshProUGUI textf in textFields){
+            textf.text=textf.text.Replace("Loup-garou","Werewolf");
+            textf.text=textf.text.Replace("Voyante","Seer");
+            textf.text=textf.text.Replace("Dictateur","Dictator");
+            textf.text=textf.text.Replace("Cupidon","Cupid");
+            textf.text=textf.text.Replace("Villageois","Villager");
+            textf.text=textf.text.Replace("Sorciere","Witch");
+            textf.text=textf.text.Replace("Chasseur","Hunter");
+            textf.text=textf.text.Replace("Garde","Guard");
+        }
+
+
+    }
     //Fonctions pour les sons ->
     //Son à jouer pendant la nuit
     public void play_sound_night(){
@@ -655,7 +734,83 @@ public class GameManager : MonoBehaviour
         TextMeshProUGUI newText;
         GameObject newPlayer = Instantiate(textWinPlayer, panel.transform);
         newText = newPlayer.GetComponent<TextMeshProUGUI>();
-        newText.text = listPlayer[num].GetPseudo() + ": " + listPlayer[num].GetRole();
+        string nomrole_translated="";
+        switch(listPlayer[num].GetRole()){
+            case "Loup-garou":
+                    if(isfr){
+                        nomrole_translated="Loup-garou";
+                    }
+                    else{
+                        nomrole_translated="Werewolf";
+                    }
+                    Debug.Log("Loup ");
+                    break;
+                case "Villageois":
+                    if(isfr){
+                        nomrole_translated="Villageois";
+                    }
+                    else{
+                        nomrole_translated="Villager";
+                    }
+                    break;
+                case "Cupidon":
+                    if(isfr){
+                        nomrole_translated="Cupidon";
+                    }
+                    else{
+                        nomrole_translated="Cupid";
+                    }
+                    break;
+                case "Sorciere":
+                    if(isfr){
+                        nomrole_translated="Sorciere";
+                    }
+                    else{
+                        nomrole_translated="Witch";
+                    }
+                    Debug.Log("Sorc ");
+                    break;
+                case "Voyante":
+                    if(isfr){
+                        nomrole_translated="Voyante";
+                    }
+                    else{
+                        nomrole_translated="Seer";
+                    }
+                    Debug.Log("Voyan ");
+                    break;
+                case "Chasseur":
+                    if(isfr){
+                        nomrole_translated="Chasseur";
+                    }
+                    else{
+                        nomrole_translated="Hunter";
+                    }
+                    Debug.Log("Chasseur ");
+                    break;
+                case "Dictateur":
+                    if(isfr){
+                        nomrole_translated="Dictateur";
+                    }
+                    else{
+                        nomrole_translated="Dictator";
+                    }
+                    Debug.Log("Dictateur ");
+                    break;
+                case "Garde":
+                    if(isfr){
+                        nomrole_translated="Garde";
+                    }
+                    else{
+                        nomrole_translated="Guard";
+                    }
+                    Debug.Log("Garde ");
+                    break;
+            Debug.Log(listPlayer[num].GetRole());
+        }
+
+
+        newText.text = listPlayer[num].GetPseudo() + ": " + nomrole_translated;
         Debug.Log(listPlayer[num].GetRole());
         if (listPlayer[num].GetRoleId() == 4)
         {
@@ -685,22 +840,42 @@ public class GameManager : MonoBehaviour
 
         switch(isVillageWin) {
             case 2 : // Loup-garou
-                groupWin.text = "Werewolves won";
+                if(isfr){
+                    groupWin.text = "Les loups-garous ont gagné";
+                }
+                else{
+                    groupWin.text = "Werewolves won";
+                }
                 groupWin.color = colorRed;
                 break;
             
             case 1 : // villageois
-                groupWin.text = "Villagers won";
+                if(isfr){
+                    groupWin.text = "Les villageois ont gagné";
+                }
+                else{
+                    groupWin.text = "Villagers won";
+                }
                 groupWin.color = colorWhite;
                 break;
             
             case 4 : // draw
-                groupWin.text = "Draw";
+                if(isfr){
+                    groupWin.text = "Draw";
+                }
+                else{
+                    groupWin.text = "Match nul";
+                }
                 groupWin.color = colorWhite;
                 break;
 
             case 3 : // amoureux
-                groupWin.text = "Lovers won";
+                if(isfr){
+                    groupWin.text = "Les amoureux ont gagné";
+                }
+                else{
+                    groupWin.text = "Lovers won";
+                }
                 groupWin.color = colorPink;
                 break;
         }
